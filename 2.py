689c4457486d5a0d6af2fc731b728c1d5aac237c
Alex <85848281+Alex-esc1@users.noreply.github.com>
2021-07-23 10:46:29 +0300

    Парсер для olx.kz
    Рабочий скрипт
    (Название, цена и фото товаров)

---
+++ 2.py
@@ -0,0 +1,42 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def save():
+    with open('1.txt', 'a', encoding="utf-8") as file:
+        file.write(f'{comp["title"]} -> Price: {comp["price"]} -> img: {comp["img"]}\n')
+
+
+def parse(line):
+    URL = line.strip()
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(URL, headers = HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    items = soup.findAll('div', class_ = 'offer-wrapper' )
+    comps = []
+    
+    for item in items:
+        img = item.find('img', class_ = 'fleft')
+
+        imgSrc = ''
+        try:
+            imgSrc = img.get('src')
+        except:
+            pass
+
+        comps.append({
+            'title': item.find('h3', class_ = 'lheight22 margintop5').get_text(strip = True),
+            'price': item.find('p', class_ = 'price').get_text(strip = True),
+            'img': imgSrc
+        })
+        global comp
+        for comp in comps:
+            print(f'{comp["title"]} -> Price: {comp["price"]} -> img: {comp["img"]}')
+            save()
+
+
+with open("2.txt", "r", encoding="utf-8") as file1:
+    for line in file1:
+        parse (line)
