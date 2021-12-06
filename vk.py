74f37a6f040737ffc6d338cd12074caefbbc5264
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2021-12-07 01:24:32 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -28,10 +28,10 @@ driver.maximize_window()
 #Вход в соцсеть
 driver.get("http://vk.com")
 sleep(5)
-driver.find_element_by_id("index_email").send_keys('+375333122224')##вместо yourlogin вводин свой логин или номер телефона
+driver.find_element_by_id("index_email").send_keys('+375111111111')##вместо yourlogin вводин свой логин или номер телефона
 sleep(2)
 pwd = driver.find_element_by_id("index_pass")
-pwd.send_keys('74zodage')# Вводим пароль вместо YoUrPaSSwOrD
+pwd.send_keys('1111111')# Вводим пароль вместо YoUrPaSSwOrD
 pwd.send_keys(Keys.ENTER)
 sleep(7)
  
