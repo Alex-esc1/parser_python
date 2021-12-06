c4d562e3e5928dbb4f8d7adf52a6440484d66359
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2021-12-07 01:21:57 +0300

    posting vk in groups
    В файл 1.txt необходимо вставить ссылки групп (каждая с новой строки)
    В файле blogurl.txt ставите ссылку на фото или пост, который нужно разместить

---
+++ vk_posting.py
@@ -0,0 +1,62 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.options import Options
+from time import sleep
+import requests
+
+##Чтение ссылки из текстового файла
+f = open('blogurl.txt', 'r')
+blogurl = f.readline()
+blogurl=blogurl+' '
+kolbs = len(blogurl)
+print ('Blog url: '+blogurl)
+ 
+print("\nConnection success...")
+## настройки мозила
+options = Options()
+options.headless = True
+driver = webdriver.Firefox(executable_path=r"C:/test/geckodriver.exe")
+driver.maximize_window()
+ 
+ 
+#Вход в соцсеть
+driver.get("http://vk.com")
+sleep(5)
+driver.find_element_by_id("index_email").send_keys('+375333122224')##вместо yourlogin вводин свой логин или номер телефона
+sleep(2)
+pwd = driver.find_element_by_id("index_pass")
+pwd.send_keys('74zodage')# Вводим пароль вместо YoUrPaSSwOrD
+pwd.send_keys(Keys.ENTER)
+sleep(7)
+ 
+driver.implicitly_wait(30)
+
+with open("1.txt", "r", encoding="utf-8") as file:
+    for line in file:
+      URL = line.strip()
+      driver.get(URL)
+      sleep(7)
+      e = driver.find_element_by_id("post_field")
+      e.clear()
+      act = ActionChains(driver)
+      act.move_to_element(e).perform()
+      act = ActionChains(driver)
+      act.click(e).perform()
+      sleep(3)
+      act = ActionChains(driver)
+      act.send_keys(blogurl).perform()
+      sleep(3)
+      for _ in range(kolbs):
+        act = ActionChains(driver)
+        act.send_keys(Keys.BACKSPACE).perform()
+      driver.find_element_by_id("send_post").click()
+      sleep(3)
+
+#закрываем браузер
+driver.close()
