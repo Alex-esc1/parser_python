7e93268dbb09d439868b37036c891db59efceaa1
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2021-12-09 22:17:58 +0300

    добавил оповещения
    add print

--- vk_print.py
+++ vk_print.py
@@ -29,10 +29,10 @@ driver.maximize_window()
 #Вход в соцсеть
 driver.get("http://vk.com")
 sleep(5)
-driver.find_element_by_id("index_email").send_keys('+375333122224')##вместо yourlogin вводин свой логин или номер телефона
+driver.find_element_by_id("index_email").send_keys('+375')##вместо yourlogin вводин свой логин или номер телефона
 sleep(2)
 pwd = driver.find_element_by_id("index_pass")
-pwd.send_keys('74zodage')# Вводим пароль вместо YoUrPaSSwOrD
+pwd.send_keys('3456')# Вводим пароль вместо YoUrPaSSwOrD
 pwd.send_keys(Keys.ENTER)
 print("регистрацию прошел успешно")
 sleep(7)
