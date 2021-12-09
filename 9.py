d6faf93e4a3abbf3f92c8158ac30271058aad722
Alex Shilenkov <85848281+Alex-esc1@users.noreply.github.com>
2021-12-09 20:39:11 +0300

    Получения текущей страницы
    Получения текущей страницы с выводом в консоль и csv

---
+++ 9.py
@@ -0,0 +1,83 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def save():
+    with open('2.csv', 'a', encoding="utf-8") as file:
+        file.write(f'{comp["title"]};Price: {comp["price"]};img: {comp["img"]};description: {comp["description"]}\n')
+
+def get_text(parent, name=None, index=0, attrs={}, recursive=True, text=None, **kwargs):
+    items = parent.findAll(name, attrs, recursive, text, **kwargs)
+    if index >= len(items):
+        return ''
+    item = items[index]
+    try:
+        return item.get_text(strip = True)
+    except:
+        return ''
+
+def get_image_src(item, name=None, attrs={}, recursive=True, text=None, **kwargs):
+    text = ''
+    try:
+        text = item.find(name, attrs, recursive, text, **kwargs).get('src')
+    except:
+        pass
+
+    return text
+
+def get_href(item, name=None, attrs={}, recursive=True, text=None, **kwargs):
+    text = ''
+    try:
+        text = item.find(name, attrs, recursive, text, **kwargs).get('href')
+    except:
+        pass
+
+    return text
+
+def get_url(item, name=None, attrs={}, recursive=True, text=None, **kwargs):
+    text = ''
+    try:
+        text = item.find(name, attrs, recursive, text, **kwargs).get('url')
+    except:
+        pass
+
+    return text
+
+def parse(line):
+    URL = line.strip()
+    HEADERS = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(URL, headers = HEADERS)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #items = soup.findAll('div', class_ = 'main-page-wrapper')
+    items = soup.findAll('body') # поиск по всей странице
+    comps = []
+    
+    for item in items:
+        NameS = get_text(item, 'h1')
+        priceS = URL
+        descriptionS = get_text(item, '', class_ = 'wd-tab-wrapper')
+        imgSrc = get_href(item, '', class_ = 'wd-tab-wrapper')
+
+        #try:
+        #     imgSrc = item.get('href')
+        #except:
+        #     pass
+
+        comps.append({
+            'title': NameS,
+            'price': priceS,
+            'img': imgSrc,
+            'description': descriptionS
+        })
+
+        global comp
+        for comp in comps:
+            print(f'{comp["title"]} -> description: {comp["description"]} -> Price: {comp["price"]} -> img: {comp["img"]}')
+            save()
+
+
+with open("1.txt", "r", encoding="utf-8") as file1:
+    for line in file1:
+        parse (line)
